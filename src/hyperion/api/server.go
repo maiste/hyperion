f460c729abacd982dd21ac864bad8aba3ebaa3e9
Etienne MARAIS <etiennemarais@maiste.fr>
2019-12-06 19:43:27 +0100

    template handler

--- src/hyperion/api/server.go
+++ src/hyperion/api/server.go
@@ -23,9 +23,16 @@ func handleTest(w http.ResponseWriter, r* http.Request) {
   fmt.Fprintf(w, "SERVER ON")
 }
 
+func handleTemplate(w http.ResponseWriter, r* http.Request) {
+  request := mux.Vars(r)
+  template := request["template"]
+  fmt.Println("[REQUEST] Download", template)
+}
+
 /* Handle connextions on port 8080 */
 func ConnectionHandler() {
   route := mux.NewRouter().StrictSlash(true)
   route.HandleFunc("/", handleTest)
+  route.HandleFunc("/{template}", handleTemplate)
   log.Fatal(http.ListenAndServe(":8080", route))
 }
